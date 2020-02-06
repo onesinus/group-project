0058f671deac5b9ef0e6690d4f4214312cfd44ba
Onesinus SPT <onesinus231@gmail.com>
2020-02-06 18:36:09 +0700

    Route User

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+/* Require Routes */
+const userRoute     = require('./routes/Users');
+
+app.use('/users', userRoute);
+/* End Require Routes */
 
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`App listening on port ${port}!`)
+app.listen(port, () => console.log(`App listening on port ${port}!`));
