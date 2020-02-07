35200ca052f86ab376ae0e903fde86cda612bfa8
dedehudianto12 <dedehudianto12@gmail.com>
2020-02-07 09:23:52 +0700

    add server football get fixtures get standings addtofavorite

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+const routes = require('./routes')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', routes)
 
-app.listen(port, () => console.log(`App listening on port ${port}!`)
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
